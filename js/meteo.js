fe436c74aefe26e849ef08fffd634e1bf6d016f5
JPdwwm <gripacus.cvg@gmail.com>
2023-12-19 00:52:24 +0100

    Séparation des variables et modification du metoe.js

--- js/meteo.js
+++ js/meteo.js
@@ -1,44 +1,43 @@
 // meteo.js
 import { config } from './config.js';
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
 async function loadCityName() {
-    try {
-        const response = await fetch('./config.json');
-        const config = await response.json();
+    return fetch('./config.json')
+        .then(response => response.json())
+        .then(config => {
+            if (!config.city || !config.city.zipCode) {
+                console.error("La configuration du nom de la ville est invalide.");
+                return Promise.reject("La configuration du nom de la ville est invalide.");
+            }
 
-    if (!config.city || !config.city.name) {
-        console.error("La configuration du nom de la ville est invalide.");
-        return null;
-    }
+            return config.city.zipCode;
+        })
+        .catch(error => {
+            console.error("Erreur lors du chargement de la configuration de la ville :", error);
+            return Promise.reject("Erreur lors du chargement de la configuration de la ville : " + error.message);
+        });
+}
 
-        return config.city.name;
-    } catch (error) {
-        console.error("Erreur lors du chargement de la configuration de la ville :", error);
-        return null;
-    }}
-
-    async function checkWeather() {
-        try {
-            const cityName = await loadCityName();
-
-        if (!cityName) {
-            console.error("Nom de ville invalide.");
-            return;
-        }
-
-        const response = await fetch(apiUrl + cityName + `&appid=${config.apiKey}`);
-        const data = await response.json();
-
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°";
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-        } catch (error) {
-        console.error("Erreur lors de la récupération des données météorologiques :", error);
-        }
-    }
+function checkWeather() {
+    loadCityName()
+        .then(cityName => {
+            if (!cityName) {
+                console.error("Nom de ville invalide.");
+                return Promise.reject("Nom de ville invalide.");
+            }
+            return fetch(config.apiUrl + cityName + `&appid=` + config.apiKey);
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°";
+            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        })
+        .catch(error => {
+            console.error("Erreur lors de la récupération des données météorologiques :", error);
+        });
+}
 
     checkWeather();
 
